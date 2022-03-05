d7c2fce1401a07111faa3af63f3a556e3f0d3dc8
Brandon Burton <brandon.anthony.burton@gmail.com>
2022-03-05 20:53:56 +0000

    Adds validations for the number length & format

--- formatter/phone_number/uk.rb
+++ formatter/phone_number/uk.rb
@@ -1,24 +1,37 @@
+require_relative 'uk/validations'
 module Formatter
   module PhoneNumber
     class UK
       COUNTRY_CODE = '44'
 
-      class << self
-        def format(phone_number)
-          phone_number = phone_number.strip
+      def self.format(phone_number)
+        new(phone_number).format
+      end
 
-          if phone_number.start_with?('0')
-            phone_number = chop_first_character(phone_number)
-          end
+      def initialize(phone_number)
+        @phone_number = phone_number
+      end
 
-          phone_number.prepend("+#{COUNTRY_CODE}")
+      def format
+        @phone_number = @phone_number.strip
+
+        if @phone_number.start_with?('0')
+          @phone_number = chop_first_character
         end
 
-        private
+        validate_phone_number
 
-        def chop_first_character(phone_number)
-          phone_number[1..-1]
-        end
+        @phone_number.prepend("+#{COUNTRY_CODE}")
+      end
+
+      private
+
+      def chop_first_character
+        @phone_number[1..-1]
+      end
+
+      def validate_phone_number
+        Formatter::PhoneNumber::UK::Validations.validate(@phone_number)
       end
     end
   end
--- spec/formatter/phone_number/uk_spec.rb
+++ spec/formatter/phone_number/uk_spec.rb
@@ -9,6 +9,36 @@ module Formatter
 
         expect(described_class.format(uk_phone_number)).to eq('+447123456789')
       end
+
+      it 'returns an error if the phone number is too short' do
+        short_number = '0712345'
+
+        expect{ described_class.format(short_number) }
+          .to raise_error(
+            Formatter::PhoneNumber::UK::PhoneNumberTooShort,
+            'This number is a little too short.'
+          )
+      end
+
+      it 'returns an error if the phone number is too long' do
+        long_number = '071234567890'
+
+        expect{ described_class.format(long_number) }
+          .to raise_error(
+            Formatter::PhoneNumber::UK::PhoneNumberTooLong,
+            'This number is a little too long.'
+          )
+      end
+
+      it 'returns an error if the number does NOT begin with a 7 after the prefix' do
+        wrong_number = '09123456789'
+
+        expect{ described_class.format(wrong_number) }
+          .to raise_error(
+            Formatter::PhoneNumber::UK::InvalidMobileFormat,
+            'This is not a valid UK mobile phone number.'
+          )
+      end
     end
   end
 end
