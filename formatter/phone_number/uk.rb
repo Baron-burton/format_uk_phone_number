fae887022924985b42cbeb06db90c3dd27c614bf
Brandon Burton <brandon.anthony.burton@gmail.com>
2022-03-06 15:24:59 +0000

    Prefers instance variable to passing args around
    Just makes it a bit tidier to read rather than passing
    and re-assigning phone_number everywhere

--- formatter/phone_number/uk.rb
+++ formatter/phone_number/uk.rb
@@ -4,37 +4,41 @@ module Formatter
     class UK
       PREFIX = '+44'.freeze
 
-      class << self
-        def format(phone_number)
-          phone_number.strip!
+      def self.format(phone_number)
+        new(phone_number).format
+      end
 
-          phone_number = remove_prefixes(phone_number)
-          validate_phone_number(phone_number)
+      def initialize(phone_number)
+        @phone_number = phone_number
+      end
 
-          phone_number.prepend(PREFIX)
+      def format
+        @phone_number.strip!
+
+        remove_prefixes
+        validate_phone_number
+
+        @phone_number.prepend(PREFIX)
+      end
+
+      private
+
+      def validate_phone_number
+        Formatter::PhoneNumber::UK::Validations.validate(@phone_number)
+      end
+
+      def remove_prefixes
+        if @phone_number.start_with?('0')
+          chop_first_characters
+        elsif @phone_number.start_with?('44')
+          chop_first_characters(start_from: 2)
+        elsif @phone_number.start_with?('+44')
+          chop_first_characters(start_from: 3)
         end
+      end
 
-        private
-
-        def validate_phone_number(phone_number)
-          Formatter::PhoneNumber::UK::Validations.validate(phone_number)
-        end
-
-        def remove_prefixes(phone_number)
-          if phone_number.start_with?('0')
-            phone_number = chop_first_characters(phone_number)
-          elsif phone_number.start_with?('44')
-            phone_number = chop_first_characters(phone_number, start_from: 2)
-          elsif phone_number.start_with?('+44')
-            phone_number = chop_first_characters(phone_number, start_from: 3)
-          end
-
-          phone_number
-        end
-
-        def chop_first_characters(phone_number, start_from: 1)
-          phone_number[start_from..-1]
-        end
+      def chop_first_characters(start_from: 1)
+        @phone_number = @phone_number[start_from..-1]
       end
     end
   end
